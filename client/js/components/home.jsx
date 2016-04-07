a0c64506451dfb44abaad45670e1d53c1be8a66e
Justin Ball <justinball@gmail.com>
2016-04-07 17:37:37 -0600

    Removes unused home page

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -6,11 +6,18 @@ import assets                   from '../libs/assets';
 class Home extends React.Component {
 
   render(){
-    
+
     const img = assets("./images/atomicjolt.jpg");
 
     return<div>
-      <img src={img} />
+      <h1>React Firebase Starter App</h1>
+        {this.props.children}
+        <div className="footer">
+          <p>
+            <img src={img} />
+            Built by <a href="http://www.atomicjolt.com">Atomic Jolt</a>.
+          </p>
+        </div>
     </div>;
   }
 
